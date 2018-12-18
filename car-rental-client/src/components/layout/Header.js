856136d6492dc7a29c57907f23ea550e9c099901
Michał Nowakowski <michal951@poczta.onet.pl>
2018-12-18 14:22:32 +0100

    added components folder with header.js inside which provide navbar from bootstrap

---
+++ car-rental-client/src/components/layout/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav
+        className="navbar navbar-expand-lg navbar navbar-dark bg-dark"
+        style={{
+          alignItems: "center",
+          display: "flex"
+        }}
+      >
+        <a className="navbar-brand" href="#">
+          Car rental
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link" href="#">
+              Home
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Cars
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Pricing
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
