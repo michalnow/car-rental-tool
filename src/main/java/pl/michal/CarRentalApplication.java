9de664a0e5cda1dc23636e3e688d00ad46fd0063
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-15 18:32:40 +0100

    added class which task is to save test data to h2 database

--- src/main/java/pl/michal/CarRentalApplication.java
+++ src/main/java/pl/michal/CarRentalApplication.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Component;
 import pl.michal.carcatalog.Car;
 import pl.michal.carcatalog.CarRepository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 @SpringBootApplication
 public class CarRentalApplication {
@@ -16,15 +20,40 @@ public class CarRentalApplication {
 	CarRepository repo;
 
 	@Component
-	private class DevFixtures implements  CommandLineRunner{
+	private class DevFixtures implements CommandLineRunner{
+
+		String pattern = "yyyy-mm-dd";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+		Date date;
+
+		{
+			try {
+				date = simpleDateFormat.parse("2019-01-30");
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+
 
 		@Override
 		public void run(String... args) throws Exception {
-			//repo.save(Car.builder()
-				//	.name("sadsda")
-				//	.age(29)
-					//.asd
-					//.build());
+			repo.save(Car.builder().carName("Ferrari").carIdentifier("KR 72AXC")
+					.carModel("F50").engineType("V8").fuelType("Petrol").milage(543212)
+					.rating(10).transmission("sequential").typeOfDrive("RWD").yearOfProduction(date).isRented("no").noOfSeats(4).trunk(100).build());
+
+			repo.save(Car.builder().carName("Audi").carIdentifier("KR 872HG")
+					.carModel("A4").engineType("2.0").fuelType("Petrol").milage(75241)
+					.rating(8).transmission("manual").typeOfDrive("RWD").yearOfProduction(date).isRented("no").noOfSeats(5).trunk(300).build());
+
+
+			repo.save(Car.builder().carName("Skoda").carIdentifier("KR OKUT2")
+					.carModel("Fabia").engineType("1,4").fuelType("Gas").milage(128754)
+					.rating(6).transmission("manual").typeOfDrive("FWD").yearOfProduction(date).isRented("no").noOfSeats(5).trunk(420).build());
+
+			repo.save(Car.builder().carName("Mercedes").carIdentifier("KR HYE12")
+					.carModel("Benz C").engineType("V6").fuelType("Petrol").milage(98521)
+					.rating(10).transmission("Automatic").typeOfDrive("RWD").yearOfProduction(date).isRented("no").noOfSeats(4).trunk(200).build());
+
 		}
 	}
 	public static void main(String[] args) {
