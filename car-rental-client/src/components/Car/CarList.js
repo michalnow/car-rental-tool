c76456207da9a9e24bff27c496b35ca13e606851
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-16 16:28:44 +0100

    change layout of page to table instead of list and added link to redirect to details of the car

--- car-rental-client/src/components/Car/CarList.js
+++ car-rental-client/src/components/Car/CarList.js
@@ -21,42 +21,44 @@ export default class CarList extends Component {
   render() {
     return (
       <div className="d-flex">
-        <ul className="list-group mx-auto list-group-flush">
-          {this.state.cars.map((car, i) => (
-            <div className="container" key={i}>
-              <div className="row">
-                <div className="col-sm">
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                    <h3>
-                      <Link
-                        style={{
-                          backgroundColor: "#343A40",
-                          color: "white",
-                          padding: "4px 25px",
-                          textAlign: "center",
-                          textDecoration: "none",
-                          display: "inline-block"
-                        }}
-                        to={`/cars/details/${car.carIdentifier}`}
-                      >
-                        {car.carName}&nbsp;{car.carModel}&nbsp;{car.engineType}
-                      </Link>
-                    </h3>
-                  </li>
-                </div>
-                <div className="col-sm">
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                    <h3>
-                      <span className="badge badge-warning badge-pill">
-                        rating {car.rating} / 10
-                      </span>
-                    </h3>
-                  </li>
-                </div>
-              </div>
-            </div>
-          ))}
-        </ul>
+        <table
+          className="table table-striped table-light"
+          style={{
+            marginTop: "10px",
+            fontSize: "24px"
+          }}
+        >
+          <thead>
+            <tr>
+              <th scope="col">Car</th>
+              <th scope="col">Price per day</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.cars.map((car, i) => (
+              <tr>
+                <td>
+                  {car.carName}&nbsp;{car.carModel}&nbsp;{car.engineType}
+                </td>
+                <td>
+                  <h3>
+                    <span className="badge badge-warning badge-pill">
+                      rating {car.rating} / 10
+                    </span>
+                  </h3>
+                </td>
+                <td>
+                  <Link
+                    to={`/cars/details/${car.carIdentifier}`}
+                    style={{ color: "black" }}
+                  >
+                    Check it out
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
