99605ffda466b1f89c09e20fd2b4e9cf1ae617a1
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-16 11:24:47 +0100

    change layout to 2 columns and change table to only have th tag and added Slider under the table

--- car-rental-client/src/components/Car/CarDetails.js
+++ car-rental-client/src/components/Car/CarDetails.js
@@ -1,5 +1,40 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Slider from "react-slick";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "transparent",
+        marginRight: "50px"
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "transparent",
+        marginLeft: "50px",
+        zIndex: "1",
+        fontSize: "20px"
+      }}
+      onClick={onClick}
+    />
+  );
+}
 
 export default class CarDetails extends Component {
   constructor(props) {
@@ -19,74 +54,149 @@ export default class CarDetails extends Component {
       this.setState({ car });
       console.log(this.state.car);
       console.log(this.state.car.carName);
+      window.scrollTo(0, 0);
     });
   }
 
   render() {
+    const settings = {
+      dots: false,
+      pauseOnHover: false,
+      arrows: false,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
+    };
+
     return (
       <div class="container">
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "22px",
+            marginTop: "5px",
+            marginBottom: "5px"
+          }}
+        >
+          <button type="button" className="btn-lg btn-outline-dark " style={{}}>
+            &nbsp;&nbsp;&nbsp;Rent now ! &nbsp;&nbsp;&nbsp;
+          </button>
+        </div>
         <div class="row">
-          <div class="col-sm">One of three columns</div>
-          <div class="col-sm">
-            <table class="table" style={{ fontSize: "24px" }}>
-              <thead>
-                <tr>
-                  <td colSpan="2">This is your selected car </td>
-                </tr>
-              </thead>
+          <div class="col-sm" style={{ marginLeft: "10px" }}>
+            <table class="table" style={{ fontSize: "18px" }}>
               <tbody>
                 <tr>
                   <th scope="row">Brand</th>
-                  <td>{this.state.car.carName}</td>
                 </tr>
                 <tr>
                   <th scope="row">Model</th>
-                  <td>{this.state.car.carModel}</td>
                 </tr>
                 <tr>
                   <th scope="row">Engine</th>
-                  <td>{this.state.car.engineType}</td>
                 </tr>
                 <tr>
                   <th scope="row">Fuel</th>
-                  <td>{this.state.car.fuelType}</td>
                 </tr>
                 <tr>
                   <th scope="row">Transmission</th>
-                  <td>{this.state.car.transmission}</td>
                 </tr>
                 <tr>
                   <th scope="row">Type of drive</th>
-                  <td>{this.state.car.typeOfDrive}</td>
                 </tr>
                 <tr>
                   <th scope="row">Year of production</th>
-                  <td>{this.state.car.yearOfProduction}</td>
                 </tr>
                 <tr>
                   <th scope="row">Milage</th>
-                  <td>{this.state.car.milage} km</td>
-                </tr>
-                <tr>
-                  <th scope="row">Rented</th>
-                  <td>{this.state.car.isRented} </td>
                 </tr>
                 <tr>
                   <th scope="row">Seats</th>
-                  <td>{this.state.car.noOfSeats}</td>
                 </tr>
                 <tr>
                   <th scope="row">Trunk capacity</th>
-                  <td>{this.state.car.trunk} L</td>
                 </tr>
               </tbody>
             </table>
-            <button type="button" class="btn-lg btn-outline-dark " style={{}}>
-              &nbsp;&nbsp;&nbsp;Rent &nbsp;&nbsp;&nbsp;
-            </button>
           </div>
-          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">
+            <table
+              class="table"
+              style={{ fontSize: "18px", alignItems: "left" }}
+            >
+              <tbody>
+                <tr>
+                  <th scope="row">{this.state.car.carName}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.carModel}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.engineType}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.fuelType}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.transmission}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.typeOfDrive}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.yearOfProduction}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.milage} km</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.noOfSeats}</th>
+                </tr>
+                <tr>
+                  <th scope="row">{this.state.car.trunk} L</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "22px"
+          }}
+        >
+          Check this out !
+        </div>
+        <Slider {...settings}>
+          <div>
+            <img
+              className="d-block w-100"
+              src={require("../../images/ferrarif50.jpg")}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="d-block w-100"
+              src={require("../../images/ferrariinterior.jpg")}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="d-block w-100"
+              src={require("../../images/ferrariEngine.jpg")}
+              alt=""
+            />
+          </div>
+        </Slider>
       </div>
     );
   }
