7882eb24ecf06b758712cb7807fa901436942c9d
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-22 00:07:59 +0100

    added react frontend

---
+++ src/main/resources/static/precache-manifest.49c9a04c3e018583a64fd7966e70f711.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bb615fed6a922964a20ff20bdd688b7",
+    "url": "/static/media/ferrariinterior.6bb615fe.jpg"
+  },
+  {
+    "revision": "15d94b502447e0b3be86",
+    "url": "/static/css/main.ac727731.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "cfaef24259e30b476d06",
+    "url": "/static/js/1.cfaef242.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c64f5182d3d81ddfd512dc6fc780929f",
+    "url": "/static/media/roys.c64f5182.jpg"
+  },
+  {
+    "revision": "15d94b502447e0b3be86",
+    "url": "/static/js/main.15d94b50.chunk.js"
+  },
+  {
+    "revision": "f33e6d3e4e1bba99474c422ed093f452",
+    "url": "/static/media/camaro.f33e6d3e.jpg"
+  },
+  {
+    "revision": "bf5446b367e0354eec2c8165de130a6e",
+    "url": "/static/media/bmw.bf5446b3.jpg"
+  },
+  {
+    "revision": "f7603abc35133d69f41e35a524f92699",
+    "url": "/static/media/ferrariEngine.f7603abc.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "cfaef24259e30b476d06",
+    "url": "/static/css/1.9c41531f.chunk.css"
+  },
+  {
+    "revision": "d028a5ba76b6a00855d43c9aca22b16c",
+    "url": "/index.html"
+  }
+];
