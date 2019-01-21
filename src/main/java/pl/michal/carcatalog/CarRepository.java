fc0e4059da67055481e69d3c97e779792ec01420
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-21 10:56:34 +0100

    changed header of method to take one Sort parameter

--- src/main/java/pl/michal/carcatalog/CarRepository.java
+++ src/main/java/pl/michal/carcatalog/CarRepository.java
@@ -1,16 +1,19 @@
 package pl.michal.carcatalog;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+
 	
 	public Car findByCarIdentifier(String carIdenitfier);
 	
-	@Override
-	public Iterable<Car> findAll();
-	
+
+	public Iterable<Car> findAll(Sort a);
+
 
 }
