d7be32e9c7af4123f0c5b28e3854de545b68d408
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-30 21:09:50 +0100

    created class to descrie Payment

--- src/main/java/pl/michal/rental/Payment.java
+++ src/main/java/pl/michal/rental/Payment.java
@@ -1,4 +1,13 @@
 package pl.michal.rental;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Payment {
+    private String id;
+    private String finalUrl;
+
+    public String getFinalUrl() {
+        return finalUrl;
+    }
 }
