c29932e7cf0dd73893b0b9fb8684bd87cbf3ca02
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-30 21:09:22 +0100

    created class to describe Confirmation of reservation

--- src/main/java/pl/michal/rental/ReservationConfirmation.java
+++ src/main/java/pl/michal/rental/ReservationConfirmation.java
@@ -1,4 +1,15 @@
 package pl.michal.rental;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationConfirmation {
+   private String reservationId;
+   private String paymentUrl;
 }
