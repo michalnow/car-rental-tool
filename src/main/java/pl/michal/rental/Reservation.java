5e27ad8114d3d1261e88129e7585c75f066d5599
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-30 21:08:00 +0100

    created class to describe reservation details

--- src/main/java/pl/michal/rental/Reservation.java
+++ src/main/java/pl/michal/rental/Reservation.java
@@ -1,4 +1,18 @@
 package pl.michal.rental;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Reservation {
+    String id;
+    String carId;
+    int days;
+    double price;
+    Payment payment;
 }
