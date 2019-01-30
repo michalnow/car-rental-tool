531c80e46dfee19db6e98dfbcb9076b56cf8ef82
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-30 21:08:58 +0100

    added class to get reservationRequest data

--- src/main/java/pl/michal/rental/ReservationRequest.java
+++ src/main/java/pl/michal/rental/ReservationRequest.java
@@ -1,4 +1,17 @@
 package pl.michal.rental;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationRequest {
+    private String carId;
+    private ClientData clientData;
+    private int days;
 }
