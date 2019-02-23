6ac6326096aa28688a9654870c4df3ae48bd0f68
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-23 13:18:40 +0100

    added errors object to state, created cycle hook for willReceiveProps, created function mapStateToProps and implemented classnames for all input fields to display nice looking errors

--- car-rental-client/src/components/Car/AddCar.js
+++ car-rental-client/src/components/Car/AddCar.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createCar } from "../../actions/carActions";
+import classnames from "classnames";
+
 class AddCar extends Component {
   constructor() {
     super();
@@ -19,8 +21,15 @@ class AddCar extends Component {
       isRented: "no",
       noOfSeats: "",
       trunk: "",
-      pricePerDay: ""
+      pricePerDay: "",
+      errors: {}
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   handleChange = event => {
@@ -53,56 +62,78 @@ class AddCar extends Component {
   };
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="container" style={{ marginTop: 20 }}>
         <div className="row">
           <div className="col-sm" />
-          <div className="col-sm">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-6 text-center">Add new car form</h1>
             <form onSubmit={this.handleSubmit}>
               <div className="form-row">
                 <div className="form-group col-md-6">
                   <label htmlFor="brand">Car brand</label>
                   <input
                     type="text"
-                    className="form-control"
+                    className={classnames("form-control ", {
+                      "is-invalid": errors.carName
+                    })}
                     id="brand"
                     name="carName"
                     placeholder="brand"
                     onChange={this.handleChange}
                   />
+                  {errors.carName && (
+                    <div className="invalid-feedback">{errors.carName}</div>
+                  )}
                 </div>
                 <div className="form-group col-md-6">
                   <label htmlFor="model">Model</label>
                   <input
                     type="text"
-                    className="form-control"
+                    className={classnames("form-control", {
+                      "is-invalid": errors.carModel
+                    })}
                     id="model"
                     name="carModel"
                     placeholder="model"
                     onChange={this.handleChange}
                   />
+                  {errors.carModel && (
+                    <div className="invalid-feedback">{errors.carModel}</div>
+                  )}
                 </div>
               </div>
               <div className="form-group">
                 <label htmlFor="carId">Car identifier</label>
                 <input
                   type="text"
-                  className="form-control"
+                  className={classnames("form-control", {
+                    "is-invalid": errors.carIdentifier
+                  })}
                   id="carId"
                   name="carIdentifier"
                   onChange={this.handleChange}
                 />
+                {errors.carIdentifier && (
+                  <div className="invalid-feedback">{errors.carIdentifier}</div>
+                )}
               </div>
               <div className="form-row">
                 <div className="form-group col-md-3">
                   <label htmlFor="engine">engine</label>
                   <input
                     type="text"
-                    className="form-control"
+                    className={classnames("form-control", {
+                      "is-invalid": errors.engineType
+                    })}
                     id="engine"
                     name="engineType"
                     onChange={this.handleChange}
                   />
+                  {errors.engineType && (
+                    <div className="invalid-feedback">{errors.engineType}</div>
+                  )}
                 </div>
                 <div className="form-group col-md-4">
                   <label htmlFor="drive">type of drive</label>
@@ -153,25 +184,36 @@ class AddCar extends Component {
                     type="number"
                     min="0"
                     max="9999999"
-                    className="form-control"
+                    className={classnames("form-control", {
+                      "is-invalid": errors.milage
+                    })}
                     id="milage"
                     name="milage"
                     size="20"
                     onChange={this.handleChange}
                   />
+                  {errors.milage && (
+                    <div className="invalid-feedback">{errors.milage}</div>
+                  )}
                 </div>
               </div>
+
               <div className="form-row">
                 <div className="form-group col-md-6">
                   <label htmlFor="year">Number of seats</label>
                   <input
                     type="number"
-                    className="form-control"
+                    className={classnames("form-control", {
+                      "is-invalid": errors.noOfSeats
+                    })}
                     id="seats"
                     name="noOfSeats"
                     placeholder=""
                     onChange={this.handleChange}
                   />
+                  {errors.noOfSeats && (
+                    <div className="invalid-feedback">{errors.noOfSeats}</div>
+                  )}
                 </div>
                 <div className="form-group col-md-6">
                   <label htmlFor="rating">Trunk capacity</label>
@@ -179,25 +221,37 @@ class AddCar extends Component {
                     type="number"
                     min="0"
                     max="10000"
-                    className="form-control"
+                    className={classnames("form-control", {
+                      "is-invalid": errors.trunk
+                    })}
                     id="trunk"
                     name="trunk"
                     placeholder="Liters"
                     onChange={this.handleChange}
                   />
+                  {errors.trunk && (
+                    <div className="invalid-feedback">{errors.trunk}</div>
+                  )}
                 </div>
               </div>
               <div className="form-row">
                 <div className="form-group col-md-6">
                   <label htmlFor="year">Production year</label>
                   <input
-                    type="text"
-                    className="form-control"
+                    type="date"
+                    className={classnames("form-control", {
+                      "is-invalid": errors.yearOfProduction
+                    })}
                     id="year"
                     name="yearOfProduction"
                     placeholder="YYYY-MM-DD"
                     onChange={this.handleChange}
                   />
+                  {errors.yearOfProduction && (
+                    <div className="invalid-feedback">
+                      {errors.yearOfProduction}
+                    </div>
+                  )}
                 </div>
                 <div className="form-group col-md-6">
                   <label htmlFor="rating">rating</label>
@@ -205,27 +259,37 @@ class AddCar extends Component {
                     type="number"
                     min="0"
                     max="10"
-                    className="form-control"
+                    className={classnames("form-control", {
+                      "is-invalid": errors.rating
+                    })}
                     id="rating"
                     name="rating"
                     placeholder="0-10"
                     onChange={this.handleChange}
                   />
+                  {errors.rating && (
+                    <div className="invalid-feedback">{errors.rating}</div>
+                  )}
                 </div>
               </div>
               <div className="form-group">
                 <label htmlFor="carId">Price per day</label>
                 <input
                   type="number"
-                  className="form-control"
+                  className={classnames("form-control", {
+                    "is-invalid": errors.pricePerDay
+                  })}
                   id="pricePerDay"
                   name="pricePerDay"
                   placeholder="in ZŁ"
                   onChange={this.handleChange}
                 />
+                {errors.pricePerDay && (
+                  <div className="invalid-feedback">{errors.pricePerDay}</div>
+                )}
               </div>
               <button type="submit" className="btn btn-primary">
-                &nbsp;Add &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp;Add &nbsp; &nbsp; &nbsp;&nbsp;
               </button>
             </form>
           </div>
@@ -236,11 +300,16 @@ class AddCar extends Component {
   }
 }
 
-AddCar.propTypes = () => {
-  createCar = PropTypes.func.isRequired;
+AddCar.propTypes = {
+  createCar: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { createCar }
 )(AddCar);
