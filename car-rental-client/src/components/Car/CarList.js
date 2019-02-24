450fbce26c75cfb0e46baf26ca6ae60db771809f
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-24 10:35:32 +0100

    refactored the code to needs of redux, instead of displaying cars in table, now there are listed as CarItem in card bootstrap layout

--- car-rental-client/src/components/Car/CarList.js
+++ car-rental-client/src/components/Car/CarList.js
@@ -1,65 +1,41 @@
 import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getCars } from "../../actions/carActions";
+import PropTypes from "prop-types";
+import CarItem from "./CarItem";
 
-export default class CarList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: []
-    };
-  }
-
+class CarList extends Component {
   componentDidMount() {
-    axios.get("http://localhost:8080/api/car/all").then(res => {
-      const cars = res.data;
-      this.setState({ cars });
-      console.log(cars);
-    });
+    this.props.getCars();
   }
 
   render() {
+    const { cars } = this.props.car;
     return (
-      <div className="d-flex">
-        <table
-          className="table table-striped table-light"
-          style={{
-            marginTop: "10px",
-            fontSize: "24px"
-          }}
-        >
-          <thead>
-            <tr>
-              <th scope="col">Car</th>
-              <th scope="col">Price per day</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.cars.map((car, i) => (
-              <tr>
-                <td>
-                  {car.carName}&nbsp;{car.carModel}&nbsp;{car.engineType}
-                </td>
-                <td>
-                  <h3>
-                    <span className="badge badge-warning badge-pill">
-                      rating {car.rating} / 10
-                    </span>
-                  </h3>
-                </td>
-                <td>
-                  <Link
-                    to={`/cars/details/${car.carIdentifier}`}
-                    style={{ color: "black" }}
-                  >
-                    Check it out
-                  </Link>
-                </td>
-              </tr>
+      <div className="container">
+        <div className="container">
+          <div className="col-sm-12">
+            <h2 className="display-4 text-center">Our awesome cars</h2>
+            {cars.map(car => (
+              <CarItem key={car.id} car={car} />
             ))}
-          </tbody>
-        </table>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+CarList.propTypes = {
+  car: PropTypes.object.isRequired,
+  getCars: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  car: state.car
+});
+
+export default connect(
+  mapStateToProps,
+  { getCars }
+)(CarList);
