77a0d8a1b99af07eb4f61eb614917346bb6b55f0
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-24 10:39:33 +0100

    created two more actions to getCars() and to getCar()

--- car-rental-client/src/actions/carActions.js
+++ car-rental-client/src/actions/carActions.js
@@ -1,14 +1,34 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_CARS, GET_CAR } from "./types";
 
 export const createCar = (car, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/car", car);
     history.push("/cars");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
       payload: err.response.data
     });
   }
+};
+
+export const getCars = () => async dispatch => {
+  const res = await axios.get("http://localhost:8080/api/car/all");
+  dispatch({
+    type: GET_CARS,
+    payload: res.data
+  });
+};
+
+export const getCar = (carIdentifier, history) => async dispatch => {
+  const res = await axios.get(`http://localhost:8080/api/car/${carIdentifier}`);
+  dispatch({
+    type: GET_CAR,
+    payload: res.data
+  });
 };
