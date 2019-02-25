c843c0d124e75acf3d3ff8e912ce795093fc21cb
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-26 00:35:09 +0100

    handling login request and jwtLogin success

--- src/main/java/pl/michal/payload/JWTLoginSuccessResponse.java
+++ src/main/java/pl/michal/payload/JWTLoginSuccessResponse.java
@@ -1,4 +1,15 @@
 package pl.michal.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class JWTLoginSuccessResponse {
+    private boolean success;
+    private String token;
 }
