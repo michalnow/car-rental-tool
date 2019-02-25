c4cd4ad229f0b1c0beed3d7b0992ecb1b99d0b06
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-26 00:48:59 +0100

    added SET_CURRENT_USER action type

--- car-rental-client/src/actions/types.js
+++ car-rental-client/src/actions/types.js
@@ -2,3 +2,5 @@ export const GET_ERRORS = "GET_ERROS";
 export const GET_CARS = "GET_CARS";
 export const GET_CAR = "GET_CAR";
 export const DELETE_CAR = "DELETE_CAR";
+
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
