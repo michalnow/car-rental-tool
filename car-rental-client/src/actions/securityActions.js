af3373a4672ba539343af20a1637b65162f777e1
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-26 00:47:21 +0100

    created security actions to handling login request, creating new user

---
+++ car-rental-client/src/actions/securityActions.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
+
+export const createNewUser = (newUser, history) => async dispatch => {
+  try {
+    await axios.post("/api/users/register", newUser);
+    history.push("/login");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const login = LoginRequest => async dispatch => {
+  try {
+    //post => Login Request
+    const res = await axios.post("/api/users/login", LoginRequest);
+    //extract token from res.data
+    const { token } = res.data;
+    //store the token in the localStorage
+    localStorage.setItem("jwtToken", token);
+    //set our token in headers
+    setJWTToken(token);
+    //decode token on React
+    const decoded = jwt_decode(token);
+
+    //dispatch to our securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const logout = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  setJWTToken(false);
+  dispatch({
+    type: SET_CURRENT_USER,
+    payload: {}
+  });
+};
+
+export const createUser = (user, history) => async dispatch => {
+  try {
+    const res = await axios.post("/api/users/update", user);
+    history.push("/cars");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
