bbcb0d1fd890436a0f235a3c9651793c66cb515e
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-25 17:43:41 +0100

    added actions to perfrom CRUD operations

--- car-rental-client/src/actions/carActions.js
+++ car-rental-client/src/actions/carActions.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { GET_ERRORS, GET_CARS, GET_CAR } from "./types";
+import { GET_ERRORS, GET_CARS, GET_CAR, DELETE_CAR } from "./types";
 
 export const createCar = (car, history) => async dispatch => {
   try {
-    const res = await axios.post("http://localhost:8080/api/car", car);
+    const res = await axios.post("/api/car", car);
     history.push("/cars");
     dispatch({
       type: GET_ERRORS,
@@ -18,7 +18,7 @@ export const createCar = (car, history) => async dispatch => {
 };
 
 export const getCars = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/car/all");
+  const res = await axios.get("/api/car/all");
   dispatch({
     type: GET_CARS,
     payload: res.data
@@ -26,9 +26,19 @@ export const getCars = () => async dispatch => {
 };
 
 export const getCar = (carIdentifier, history) => async dispatch => {
-  const res = await axios.get(`http://localhost:8080/api/car/${carIdentifier}`);
+  const res = await axios.get(`/api/car/${carIdentifier}`);
   dispatch({
     type: GET_CAR,
     payload: res.data
   });
+};
+
+export const deleteCar = carIdentifier => async dispatch => {
+  if (window.confirm("Are you sure to delete this car ?")) {
+    await axios.delete(`/api/car/${carIdentifier}`);
+    dispatch({
+      type: DELETE_CAR,
+      payload: carIdentifier
+    });
+  }
 };
