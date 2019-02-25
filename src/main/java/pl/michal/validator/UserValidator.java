da6f8605f3af177d000d0fb40cdd7a4cb18b4630
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-26 00:41:55 +0100

    Created validator for matching password with confirmPassword field

--- src/main/java/pl/michal/validator/UserValidator.java
+++ src/main/java/pl/michal/validator/UserValidator.java
@@ -1,4 +1,30 @@
 package pl.michal.validator;
 
-public class UserValidator {
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import pl.michal.userManagement.User;
+
+@Component
+public class UserValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object object, Errors errors) {
+        User user = (User) object;
+
+        if(user.getPassword().length() < 6 ){
+            errors.rejectValue("password", "Length" , "Password must be at least 6 characters");
+        }
+
+        if(!user.getPassword().equals(user.getConfirmPassword())){
+            errors.rejectValue("confirmPassword", "Match" , "Passwords must match");
+
+        }
+
+
+    }
 }
