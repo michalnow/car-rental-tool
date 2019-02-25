4d1f6ab341b4706918b67823828e5e104f786e25
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-26 00:36:09 +0100

    created response for invalid login

--- src/main/java/pl/michal/exceptions/InvalidLoginResponse.java
+++ src/main/java/pl/michal/exceptions/InvalidLoginResponse.java
@@ -1,4 +1,16 @@
 package pl.michal.exceptions;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InvalidLoginResponse {
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse(){
+        this.username = "Invalid username";
+        this.password = "Invalid password";
+    }
 }
