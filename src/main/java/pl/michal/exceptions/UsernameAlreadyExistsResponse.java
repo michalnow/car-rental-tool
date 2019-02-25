291429b221515156023ed928e481800076c65e37
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-26 00:33:23 +0100

    created exception to handle 'username already exists'

--- src/main/java/pl/michal/exceptions/UsernameAlreadyExistsResponse.java
+++ src/main/java/pl/michal/exceptions/UsernameAlreadyExistsResponse.java
@@ -1,4 +1,12 @@
 package pl.michal.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UsernameAlreadyExistsResponse {
+    private String username;
 }
