05bd116b84b9e131f6e031618f16583b48f8f6d1
Michał Nowakowski <michal951@poczta.onet.pl>
2019-02-25 17:42:52 +0100

    minor changes in order to payments handling

--- car-rental-client/src/components/Stripe/CheckoutForm.js
+++ car-rental-client/src/components/Stripe/CheckoutForm.js
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
-import { CardElement, injectStripe } from "react-stripe-elements";
+import { injectStripe } from "react-stripe-elements";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const CURRENCY = "USD";
+
+const successPayment = data => {
+  alert("Payment succesful !");
+};
+
+const badPayment = data => {
+  alert("Payment error !");
+};
+
+const fixAmount = amount => amount * 100;
+
+const onToken = (amount, description) => token => {
+  axios
+    .post("/payment/charge", {
+      description,
+      source: token.id,
+      currency: CURRENCY,
+      amount: fixAmount(amount)
+    })
+    .then(successPayment)
+    .catch(badPayment);
+};
+
+const Checkout = ({ amount }) => (
+  <StripeCheckout
+    stripeKey="pk_test_dpWQ1A2o3bDVW8vbb1QeBZyB"
+    image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png"
+    locale="auto"
+    name="Awesome Car Rental"
+    description="The most realiable cars"
+    amount={amount}
+    token={this.submit}
+    panelLabel="Rent for {{amount}}"
+  />
+);
 
 class CheckoutForm extends Component {
-  constructor(props) {
+  /*  constructor(props) {
     super(props);
     this.state = { complete: false };
     this.submit = this.submit.bind(this);
@@ -11,7 +49,7 @@ class CheckoutForm extends Component {
 
   async submit(ev) {
     let { token } = await this.props.stripe.createToken({ name: "Name" });
-    let { response } = await fetch("http://localhost:8080/payment/charge", {
+    let { response } = await fetch("/payment/charge", {
       method: "POST",
       headers: { "Content-Type": "text/plain" },
       body: token.id
@@ -21,10 +59,17 @@ class CheckoutForm extends Component {
       this.setState({ complete: true });
       this.props.history.push("/cars");
     }
-  }
+  }*/
+
+  onToken = token => {
+    fetch("/payment/charge", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain", amount: 9, token: token },
+      body: JSON.stringify(token.id)
+    });
+  };
 
   render() {
-    if (this.state.complete) return <h1>Purchase Complete</h1>;
     return (
       <StripeCheckout
         stripeKey="pk_test_dpWQ1A2o3bDVW8vbb1QeBZyB"
@@ -33,7 +78,8 @@ class CheckoutForm extends Component {
         name="Awesome Car Rental"
         description="The most realiable cars"
         amount={900}
-        token={this.submit}
+        token={this.onToken}
+        currency="PLN"
         panelLabel="Rent for {{amount}}"
       />
     );
